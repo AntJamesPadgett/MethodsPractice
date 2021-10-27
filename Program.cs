b517de974cae0c61585ed6985f0ea0d110f30613
antjamespadgett <padgettanthonyj@gmail.com>
2021-10-26 21:01:29 -0500

    Practiced Methods

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MethodsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi("Ralph", 25);
+            SayHi("Rich", 19);
+            SayHi("Sara", 32);
+
+            GoodNight("Ted");
+
+            Console.ReadLine();
+        }
+
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine($"Hello {name} you are {age}");
+        }
+
+        static void GoodNight(string name)
+        {
+            Console.WriteLine($"GoodNight {name} ");
+        }
+
+
+    }
+
+}
